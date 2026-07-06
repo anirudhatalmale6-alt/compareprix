2bd071f64fd9ccb1e417f6af347f09c67631fadd
Anirudha Talmale <anirudhatalmale6-alt@users.noreply.github.com>
2026-07-06 00:18:53 +0000

    Add basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/compareprix",
+  assetPrefix: "/compareprix/",
   images: {
     unoptimized: true,
   },
